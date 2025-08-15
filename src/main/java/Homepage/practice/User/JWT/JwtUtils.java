00d4d2dcbf3704d6159ff5ca7a9c86f2247c866f
Dev-SSW <sgg919@naver.com>
2025-08-15 16:02:57 +0900

    :sparkles: Feat: "JWT 로그인 구현 + SecurityConfig 구현"
    1. JwtUtils를 통해 JWT 토큰/리프레시 토큰 생성 및 claim 추출, 유효성, 기간 만료 검사 기능 추가
    2. JwtAuthFilter를 통해 필터를 거치지 않을 URL 추가
    3. JwtAuthFilter를 통해 JWT 토큰 검사 및 홀더에 저장 기능 구현
    4. JwtAuthFilter를 통해 토큰의 기간 만료, 유효성에 대한 예외 처리를 공통 응답으로 처리
    5. SecurityConfig를 통해 필터 체인 설정, 암호화, 인증 처리, CORS 설정 처리
    6. SecurityConfig를 통해 401, 403 예외에 관한 처리 추가

---
+++ src/main/java/Homepage/practice/User/JWT/JwtUtils.java
@@ -0,0 +1,84 @@
+package Homepage.practice.User.JWT;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Component
+public class JwtUtils {
+    private SecretKey key;
+    private static final long EXPIRATION_TIME = 86400000;           // 24 hours
+    private static final long REFRESH_EXPIRATION_TIME = 604800000;  // 7 days
+
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.key = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+    }
+
+    /** JWT 토큰 생성 */
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())                                  // "sub" 클레임에 주입
+                .setIssuedAt(new Date(System.currentTimeMillis()))                      // 발급 시간
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))  // 만료 시간
+                .signWith(key, SignatureAlgorithm.HS256)                                // HmacSHA256 키로 서명
+                .compact();
+    }
+
+    /** JWT 리프레쉬 토큰 생성 */
+    public String generateRefreshToken(HashMap<String, Object> claims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(claims)                                                      // 추가 클레임 저장 가능
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    /** 회원 ID 추출 */
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    /** 넘겨받은 클레임 정보 추출 */
+    private <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claimsResolver.apply(claims);
+    }
+
+    /** JWT 토큰 유효성 검사 */
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    /** JWT 토큰 유효성 검사 (만료 여부 제외) */
+    public boolean isTokenValidExceptExpired(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername());
+    }
+
+    /** JWT 토큰 기간 만료 검사 */
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+}
