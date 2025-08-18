271b3662bab48843ffee90f347526b1eae2a6869
Dev-SSW <sgg919@naver.com>
2025-08-18 16:31:59 +0900

    :sparkles: Feat: 스웨거 테스트를 위한 Config 추가

---
+++ src/main/java/Homepage/practice/Config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package Homepage.practice.Config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Swagger Test")
+                        .version("1.0.0")
+                        .description("<h3>Swagger test</h3>"))
+                .servers(List.of(
+                        // new Server().url("https://").description("배포 환경"),
+                        new Server().url("http://localhost:8081").description("테스트 환경")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
