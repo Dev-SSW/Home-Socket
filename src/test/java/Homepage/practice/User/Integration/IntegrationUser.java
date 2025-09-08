7408c9476b8b6ff4d8645a636098ea1c124dc1c1
Dev-SSW <sgg919@naver.com>
2025-09-08 17:45:40 +0900

    :sparkles: Feat: 유저 조회, 수정, 삭제에 관한 테스트 코드 추가
    1. UnituserService에 추가된 기능 테스트 추가
    2. UnitAuthController에 필터 비활성화를 삭제
    3. 필터 비활성화 삭제에 따라 @WithMockUser 주입 + csrf 설정
    4. @AuthenticationPrincipal 주입을 위해 .with(user(...)) 사용
    5. IntegreationUser에도 @AuthenticationPrincipal 주입을 위해 .with(user(...)) 사용
    6. .with(user(...))은 필터를 패스함

--- src/test/java/Homepage/practice/User/Integration/IntegrationUser.java
+++ src/test/java/Homepage/practice/User/Integration/IntegrationUser.java
@@ -1,8 +1,6 @@
 package Homepage.practice.User.Integration;
 
-import Homepage.practice.User.DTO.JwtRequest;
-import Homepage.practice.User.DTO.LoginRequest;
-import Homepage.practice.User.DTO.SignupRequest;
+import Homepage.practice.User.DTO.*;
 import Homepage.practice.User.JWT.JwtUtils;
 import Homepage.practice.User.User;
 import Homepage.practice.User.UserRepository;
@@ -15,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,7 +21,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.HashMap;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,11 +51,15 @@ public class IntegrationUser {
     // 테스트 필드
     private SignupRequest signupRequest;
     private LoginRequest loginRequest;
+    private UserUpdateRequest userUpdateRequest;
+    private UserPassUpdateRequest userPassUpdateRequest;
 
     @BeforeEach
     void setup() {
         signupRequest = new SignupRequest("user1", "pass1", LocalDate.of(2000,1,1), "홍길동");
         loginRequest = new LoginRequest("user1", "pass1");
+        userUpdateRequest = new UserUpdateRequest(LocalDate.of(1000,10,10), "송길똥");
+        userPassUpdateRequest = new UserPassUpdateRequest("pass1", "newPass");
     }
 
     @Test
@@ -243,5 +248,100 @@ public class IntegrationUser {
                 .andExpect(jsonPath("$.success").value(false))
                 .andExpect(jsonPath("$.message").value("토큰이 만료되었습니다."))
                 .andExpect(jsonPath("$.error").value("JWT_INVALID"));
+    }
+
+    @Test
+    @DisplayName("전체 유저 정보 가져오기 성공")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void getAllUser_success() throws Exception {
+        // 회원가입
+        User newUser = User.createUser(signupRequest, passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(newUser);
+
+        // 기존 실행 시 admin 계정을 추가해뒀음
+        mockMvc.perform(get("/admin/getAllUser"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("전체 정보 조회 성공"))
+                .andExpect(jsonPath("$.data[0].username").value("admin"))
+                .andExpect(jsonPath("$.data[1].username").value("user1"));
+    }
+
+    @Test
+    @DisplayName("특정 유저 정보 가져오기 성공")
+    void getUser_success() throws Exception {
+        // 회원가입
+        User newUser = User.createUser(signupRequest, passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(newUser);
+
+        mockMvc.perform(get("/user/getUser")
+                        .with(user(newUser)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.username").value("user1"));
+    }
+
+    @Test
+    @DisplayName("유저 정보 수정하기 성공 (비밀번호 제외)")
+    void updateUser_success() throws Exception {
+        // 회원가입
+        User newUser = User.createUser(signupRequest, passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(newUser);
+
+        mockMvc.perform(put("/user/updateUser")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userUpdateRequest))
+                        .with(user(newUser)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("정보 수정 성공"));
+    }
+
+    @Test
+    @DisplayName("비밀번호 수정하기 성공")
+    void updatePassword_success() throws Exception {
+        // 회원가입
+        User newUser = User.createUser(signupRequest, passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(newUser);
+
+        mockMvc.perform(put("/user/updatePassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userPassUpdateRequest))
+                        .with(user(newUser)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("비밀번호 수정 성공"));
+    }
+
+    @Test
+    @DisplayName("비밀번호 수정하기 실패 - 비밀번호 오류")
+    void updatePassword_fail() throws Exception {
+        // 회원가입
+        User newUser = User.createUser(signupRequest, passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(newUser);
+        UserPassUpdateRequest wrongUserPassUpdateRequest = new UserPassUpdateRequest("wrongPass", "newPass");
+
+        mockMvc.perform(put("/user/updatePassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(wrongUserPassUpdateRequest))
+                        .with(user(newUser)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value("비밀번호가 올바르지 않습니다."))
+                .andExpect(jsonPath("$.error").value("401"));
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴하기 성공 - 통합")
+    void deleteUser_success() throws Exception {
+        // 회원가입
+        User newUser = User.createUser(signupRequest, passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(newUser);
+
+        mockMvc.perform(delete("/user/deleteUser")
+                        .with(user(newUser)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("회원 탈퇴 성공"));
     }
 }
--- src/test/java/Homepage/practice/User/Unit/UnitUserService.java
+++ src/test/java/Homepage/practice/User/Unit/UnitUserService.java
@@ -1,6 +1,9 @@
 package Homepage.practice.User.Unit;
 
 import Homepage.practice.Exception.UserNotFound;
+import Homepage.practice.User.DTO.UserPassUpdateRequest;
+import Homepage.practice.User.DTO.UserResponse;
+import Homepage.practice.User.DTO.UserUpdateRequest;
 import Homepage.practice.User.Role;
 import Homepage.practice.User.User;
 import Homepage.practice.User.UserRepository;
@@ -12,8 +15,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,6 +31,8 @@ public class UnitUserService {
     // 외부 의존성 가짜 객체
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     // 테스트 할 클래스
     @InjectMocks
@@ -32,15 +40,29 @@ public class UnitUserService {
 
     // 테스트 필드
     private User testUser;
+    private User testUser2;
+    private UserUpdateRequest userUpdateRequest;
+    private UserPassUpdateRequest userPassUpdateRequest;
 
     @BeforeEach
     void setup() {
+        userUpdateRequest = new UserUpdateRequest(LocalDate.of(1000,10,10), "송길똥");
+        userPassUpdateRequest = new UserPassUpdateRequest("pass1", "newPass");
         testUser = User.builder()
                 .id(1L)
                 .username("user1")
                 .password("pass1")
                 .birth(LocalDate.of(2000,1,1))
                 .name("홍길동")
+                .role(Role.ROLE_USER)
+                .tokenVersion(1)
+                .build();
+        testUser2 = User.builder()
+                .id(1L)
+                .username("user2")
+                .password("pass2")
+                .birth(LocalDate.of(2000,1,1))
+                .name("홍길동2")
                 .role(Role.ROLE_USER)
                 .tokenVersion(1)
                 .build();
@@ -66,5 +88,80 @@ public class UnitUserService {
         assertThatThrownBy(() -> userService.loadUserByUsername("user1"))
                 .isInstanceOf(UserNotFound.class)
                 .hasMessage("아이디에 해당하는 회원이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("전체 유저 정보 가져오기 성공")
+    void getAllUser() {
+        // given
+        given(userRepository.findAll()).willReturn(List.of(testUser, testUser2));
+        // when
+        List<UserResponse> responses = userService.getAllUser();
+        // then
+        assertThat(responses.get(0).getUsername()).isEqualTo("user1");
+        assertThat(responses.get(0).getName()).isEqualTo("홍길동");
+        assertThat(responses.get(1).getUsername()).isEqualTo("user2");
+        assertThat(responses.get(1).getName()).isEqualTo("홍길동2");
+    }
+
+    @Test
+    @DisplayName("특정 유저 정보 가져오기 성공")
+    void getUser() {
+        // given
+        given(userRepository.findByUsername("user1")).willReturn(Optional.of(testUser));
+        // when
+        UserResponse response = userService.getUser("user1");
+        // then
+        assertThat(response.getUsername()).isEqualTo("user1");
+        assertThat(response.getName()).isEqualTo("홍길동");
+    }
+
+    @Test
+    @DisplayName("유저 정보 수정하기 성공 (비밀번호 제외)")
+    void updateUser() {
+        // given
+        given(userRepository.findByUsername("user1")).willReturn(Optional.of(testUser));
+        // when
+        userService.updateUser("user1", userUpdateRequest);
+        // then
+        assertThat(testUser.getName()).isEqualTo("송길똥");
+    }
+
+    @Test
+    @DisplayName("비밀번호 수정하기 성공")
+    void updatePassword_success() {
+        // given
+        given(userRepository.findByUsername("user1")).willReturn(Optional.of(testUser));
+        given(passwordEncoder.matches("pass1", "pass1")).willReturn(true);
+        given(passwordEncoder.encode("newPass")).willReturn("newPass");
+        // when
+        userService.updatePassword("user1", userPassUpdateRequest);
+        // then
+        assertThat(testUser.getPassword()).isEqualTo("newPass");
+        assertThat(testUser.getTokenVersion()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("비밀번호 수정하기 실패 - 비밀번호 오류")
+    void updatePassword() {
+        // given
+        UserPassUpdateRequest wrongUserPassUpdateRequest = new UserPassUpdateRequest("wrongPass", "newPass");
+        given(userRepository.findByUsername("user1")).willReturn(Optional.of(testUser));
+        given(passwordEncoder.matches("wrongPass", "pass1")).willReturn(false);
+        // when & then
+        assertThatThrownBy(() -> userService.updatePassword("user1", wrongUserPassUpdateRequest))
+                .isInstanceOf(BadCredentialsException.class)
+                .hasMessageContaining("비밀번호가 올바르지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴하기 성공")
+    void deleteUser() {
+        // given
+        given(userRepository.findByUsername("user1")).willReturn(Optional.of(testUser));
+        // when
+        userService.deleteUser("user1");
+        // then
+        assertThat(testUser.getTokenVersion()).isEqualTo(2); // 토큰 버전 올랐으면 실행된 것
     }
 }
