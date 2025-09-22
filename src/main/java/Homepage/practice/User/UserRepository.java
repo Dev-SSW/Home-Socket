71ffe85735b2848b23b184c7f90404ea417c143b
Dev-SSW <sgg919@naver.com>
2025-09-22 17:13:25 +0900

    :sparkles: Feat: 생일 쿠폰 자동 발급/ 쿠폰 자동 기간 만료 처리 기능 생성

--- src/main/java/Homepage/practice/User/UserRepository.java
+++ src/main/java/Homepage/practice/User/UserRepository.java
@@ -3,6 +3,8 @@ package Homepage.practice.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     /** 회원 ID가 이미 존재하는지 여부 확인 */
     boolean existsByUsername(String username);
+
+    /** 해당 생일을 가진 유저 찾기 */
+    List<User> findAllByBirthday(LocalDate today);
 }
