c840ebc430274500514ea58f0259ae0db4a11012
Dev-SSW <sgg919@naver.com>
2026-03-26 14:25:02 +0900

    :recycle: Refactor: Category 계층화
    1. 기존의 childrenIds만 가져오던 구조에서 계층화를 위해 DTO 추가
    2. Service 기능 수정
    3. BatchSize를 통해 최적화

--- src/main/java/Homepage/practice/Category/CategoryRepository.java
+++ src/main/java/Homepage/practice/Category/CategoryRepository.java
@@ -3,7 +3,13 @@ package Homepage.practice.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    /** 특정 부모 카테고리 (계층) */
+    List<Category> findByParentId(Long parentId);
 
+    /** 특정 깊이 카테고리 */
+    List<Category> findByDepth(int depth);
 }
