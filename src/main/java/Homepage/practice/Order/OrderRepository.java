c02d912e59fee1672226bdb62b48b8b20a392a86
Dev-SSW <sgg919@naver.com>
2026-03-30 22:29:40 +0900

    :recycle: Refactor: ItemRepository, OrderRepository 쿼리 수정

--- src/main/java/Homepage/practice/Order/OrderRepository.java
+++ src/main/java/Homepage/practice/Order/OrderRepository.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     /** 사용자의 주문 목록 조회 */
     @Query("select new Homepage.practice.Order.DTO.OrderListResponse(o.id, o.orderDate, o.totalPrice, d.status) " +
-            "from Order o left join o.delivery d where o.user.id = :userId")
+            "from Order o join o.delivery d where o.user.id = :userId")
     List<OrderListResponse> findOrderListByUserId(@Param("userId") Long userId);
 
     /** 상세 주문 내역 조회 */
