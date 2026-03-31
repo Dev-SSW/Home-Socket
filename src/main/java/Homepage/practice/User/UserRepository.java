df5194adb4345abfce20da7c90109496c069bd7f
Dev-SSW <sgg919@naver.com>
2026-03-31 16:41:07 +0900

    :recycle: Refactor: Coupon 최적화
    1. 모든 쿠폰 조회에 페이징 + DTO 프로젝션 적용
    2. 특정 쿠폰 조회에 DTO 프로젝션 적용

--- src/main/java/Homepage/practice/User/UserRepository.java
+++ src/main/java/Homepage/practice/User/UserRepository.java
@@ -22,7 +22,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     /** 회원 ID가 이미 존재하는지 여부 확인 */
     boolean existsByUsername(String username);
 
-    /** 해당 생일을 가진 유저 찾기 */
-    @Query("SELECT u FROM User u WHERE FUNCTION('MONTH', u.birth) = FUNCTION('MONTH', CURRENT_DATE) AND FUNCTION('DAY', u.birth) = FUNCTION('DAY', CURRENT_DATE)")
-    List<User> findAllByBirthToday();
+    /** 해당 생일을 가진 유저 ID만 찾기 */
+    @Query("SELECT u.id FROM User u WHERE FUNCTION('MONTH', u.birth) = FUNCTION('MONTH', CURRENT_DATE) AND FUNCTION('DAY', u.birth) = FUNCTION('DAY', CURRENT_DATE)")
+    List<Long> findUserIdsByBirthToday();
 }
