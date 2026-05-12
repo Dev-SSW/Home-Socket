454a777bc6e48a919ce3187e6ed5b4e2460196a9
Dev-SSW <sgg919@naver.com>
2026-05-12 17:53:26 +0900

    :sparkles: Feat: Redis 캐시 적용 (Item, Review, Category)
    1. Item, Review, Category의 조회 기능에 캐시 적용
    2. 수정, 삭제 기능에 캐시 무효화 적용
    3. Category의 list 반환을 Wrapper DTO로 감싸 객체 반환으로 변경
    4. Page를 사용하는 기능들을 PageResponse DTO로 감싸 직렬화/역질렬화 방지
    5. Redis 설정 파일

---
+++ src/main/java/Homepage/practice/Common/DTO/PageResponse.java
@@ -0,0 +1,34 @@
+package Homepage.practice.Common.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+// Page 내부 구현체가 캐시에 저장될 때 직렬화/역직렬화 문제를 해결하기 위해 단순 DTO로 변환 후 캐시에 저장
+public class PageResponse <T>{
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean first;
+    private boolean last;
+
+    public static <T> PageResponse<T> from(Page<T> page) {
+        return new PageResponse<>(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isFirst(),
+                page.isLast()
+        );
+    }
+}
---
+++ src/main/java/Homepage/practice/Config/RedisCacheConfig.java
@@ -0,0 +1,84 @@
+package Homepage.practice.Config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+// Redis 저장소를 사용하도록 설정
+public class RedisCacheConfig {
+    @Bean
+    // CacheManager : 캐시 매니저, RedisConnectionFactory : yml의 redis 설정을 통한 redis 연결 객체
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        GenericJackson2JsonRedisSerializer redisSerializer =
+                GenericJackson2JsonRedisSerializer.builder()
+                        .typeHintPropertyName("@class")         // 원래의 클래스 타입을 식별할 수 있도록 설정
+                        .build()                                                  // serializer 인스턴스 생성
+                        .configure(objectMapper -> {
+                            objectMapper.registerModule(new JavaTimeModule());                                          // 시간 관련 클래스 직렬화
+                            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);   // 날짜를 타임스탬프가 아닌 ISO-8601 문자열 형식으로 저장
+                            objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);                // 빈 객체 직렬화 시 예외 발생 방지, 프록시 객체 캐시 시 문제 방지
+                        });
+
+        // 기본 정책 만들기
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()  //조회 결과 Null이면 저장하지 않음
+                .serializeKeysWith(               // Redis key를 문자열로 저장
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new StringRedisSerializer()
+                        )
+                )
+                .serializeValuesWith(            // Redis value를 JSON으로 저장
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                redisSerializer
+                        )
+                )
+                .entryTtl(Duration.ofMinutes(5)); // 기본 TTL (Time To Live) : 캐시 유지 시간 (5분)
+
+        // 캐시 이름별로 다른 설정을 넣기 위한 Map
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+
+        cacheConfigurations.put("getRootCategory",
+                defaultConfig.entryTtl(Duration.ofHours(12)));
+
+        cacheConfigurations.put("getChildCategory",
+                defaultConfig.entryTtl(Duration.ofHours(12)));
+
+        cacheConfigurations.put("getItem",
+                defaultConfig.entryTtl(Duration.ofMinutes(3)));
+
+        cacheConfigurations.put("getAllItem",
+                defaultConfig.entryTtl(Duration.ofMinutes(1)));
+
+        cacheConfigurations.put("getItemsByCategory",
+                defaultConfig.entryTtl(Duration.ofMinutes(1)));
+
+        cacheConfigurations.put("getReview",
+                defaultConfig.entryTtl(Duration.ofMinutes(3)));
+
+        cacheConfigurations.put("getItemReview",
+                defaultConfig.entryTtl(Duration.ofMinutes(3)));
+
+        return RedisCacheManager.builder(redisConnectionFactory)    // RedisConnectionFactory로 Redis 연결
+                .cacheDefaults(defaultConfig)                                          // 기본 캐시 정책은 defaultConfig 사용
+                .withInitialCacheConfigurations(cacheConfigurations)     //  특정 캐시 정책은 cacheConfigurations TTL 사용
+                .build();
+    }
+}
+
